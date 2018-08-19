5f7d3a7cdd64257e54c73e02ed6120acf6bd9c48
Mikalai Prusenka <mikalai_prusenka@epam.com>
2018-08-19 15:42:57 +0300

    first com

---
+++ src/test/java/by/epam/RectangleTest.java
@@ -0,0 +1,56 @@
+package by.epam;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Unit test for simple Rectangle.
+ */
+public class RectangleTest
+{
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testCorner1() {
+        Rectangle rect1 = new Rectangle(0, 0, 5, 5);
+        Rectangle rect2 = new Rectangle(-2, -2, 3, 3);
+        assertTrue(rect1.intersects(rect2));
+        assertTrue(rect2.intersects(rect1));
+    }
+
+    @Test
+    public void testCorner2() {
+        Rectangle rect1 = new Rectangle(0, 0, 5, 5);
+        Rectangle rect2 = new Rectangle(3, -2, 4, 4);
+        assertTrue(rect1.intersects(rect2));
+        assertTrue(rect2.intersects(rect1));
+    }
+
+    @Test
+    public void testOutside1() {
+        Rectangle rect1 = new Rectangle(0, 0, 5, 5);
+        Rectangle rect2 = new Rectangle(5, 5, 4, 4);
+        assertFalse(rect1.intersects(rect2));
+        assertFalse(rect2.intersects(rect1));
+    }
+
+    @Test
+    public void testOutside2() {
+        Rectangle rect1 = new Rectangle(0, 0, 5, 5);
+        Rectangle rect2 = new Rectangle(3, -2, 3, 2);
+        assertFalse(rect1.intersects(rect2));
+        assertFalse(rect2.intersects(rect1));
+    }
+
+    @Test
+    public void testInside1() {
+        Rectangle rect1 = new Rectangle(0, 0, 5, 5);
+        Rectangle rect2 = new Rectangle(0, 2, 2, 2);
+        assertTrue(rect1.intersects(rect2));
+        assertTrue(rect2.intersects(rect1));
+    }
+}
